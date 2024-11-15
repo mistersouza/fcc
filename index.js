85ee77ce8da7bbb49fec90cc936baff1fdaa999f
Thiago Souza <mistersouza@icloud.com>
2024-11-15 21:55:16 +0000

    feat: connect db

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+mongoose.connect(process.env.MONGO_URI,{})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(() => console.error('Error connecting to DB'))
+  
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-
-
-
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
