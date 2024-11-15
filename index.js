372a6f903ef942db9bc8208307b9a4bbf4d546ae
Thiago Souza <mistersouza@icloud.com>
2024-11-15 14:34:14 +0000

    feat: setup and test /api/shorturl route

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/shorturl', (request, response) => [
+  response.json({
+    message: "Endpoint live and kicking"
+  })
+])
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
