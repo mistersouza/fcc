cfa6c365cb7b8e44bbaafbd54ea004fa3cadbee9
Thiago Souza <mistersouza@icloud.com>
2024-11-15 22:08:11 +0000

    feat: api/users endpoint is live and kicking

--- index.js
+++ index.js
@@ -10,11 +10,21 @@ mongoose.connect(process.env.MONGO_URI,{})
   
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', (request, response) => {
+  response.json({
+    username: request.body.username
+  })
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+
